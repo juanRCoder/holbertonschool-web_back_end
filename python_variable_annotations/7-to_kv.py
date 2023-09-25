8e398604586100d8762914b5bfd3a180514b2a4b
juanRCoder <guillecarck@gmail.com>
2023-09-25 14:26:26 -0700

    fix: fixing type-annotated function that return a tuple

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -3,7 +3,7 @@
 from typing import Tuple, Union
 
 
-def to_kv(k: str, v: Union[int, float]) -> Tuple[Union[str, float]]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """ function that return a tuple with str, int or float values """
     lst = [k, pow(v, 2)]
     return tuple(lst)
