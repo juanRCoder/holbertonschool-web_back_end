0ffb3132ab876ba9d405519f6cd9f688cf4a3058
juanRCoder <guillecarck@gmail.com>
2023-09-25 13:55:06 -0700

    feat: add function and import List from Typing to sum float values

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """ Script that return sum of float values"""
+from typing import List
 
 
-def sum_list(input_list: list) -> float:
+def sum_list(input_list: List[float]) -> float:
     """ function that returns the sum of float values"""
     return sum(input_list)
