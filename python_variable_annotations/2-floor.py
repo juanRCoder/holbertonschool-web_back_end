2dd35f094c53fbfaef14f657ae329319343427f6
juanRCoder <guillecarck@gmail.com>
2023-09-25 13:29:47 -0700

    feat: add type-annotated function that return number integer of the float

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" Script that return the number integer """
+
+
+def floor(n: float) -> int:
+    """ function that return the number integer of the number float"""
+    return int(n)
