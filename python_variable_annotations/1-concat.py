f38a1be18d3af5a5b3e0db87202c4aa0765b91de
juanRCoder <guillecarck@gmail.com>
2023-09-25 13:22:48 -0700

    feat: add fuction for concatened two variables float and return result float

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" Script tht concatenated strings """
+
+
+def concat(str1: str, str2: str) -> str:
+    """ function that concatenated two variables strings """
+    return str1 + str2
