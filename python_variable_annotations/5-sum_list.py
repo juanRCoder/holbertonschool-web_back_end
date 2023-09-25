ba38f8c2035a411181f0dc6bb81d517729bee797
juanRCoder <guillecarck@gmail.com>
2023-09-25 13:47:47 -0700

    feat: add type-annotated function that return their sum as a float

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" Script that return sum of float values"""
+
+
+def sum_list(input_list: list) -> float:
+    """ function that returns the sum of float values"""
+    return sum(input_list)
