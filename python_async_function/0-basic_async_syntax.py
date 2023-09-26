3fe3e51df4637590fb31cc4971cd6d15e3fd38e6
juanRCoder <guillecarck@gmail.com>
2023-09-26 16:41:59 -0700

    fix: adding notation in module

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -2,9 +2,10 @@
 """ Script that shows a random floating number between a given time"""
 import random
 import asyncio
+from typing import Union
 
 
-async def wait_random(max_delay=10):
+async def wait_random(max_delay: int = 10) -> Union[int, float]:
     """ function that shows a random floating """
     await asyncio.sleep(max_delay)
     return random.uniform(0, max_delay)
