b5102594011df6bf7a3088029869f0c8ed22bb28
juanRCoder <guillecarck@gmail.com>
2023-09-26 17:02:30 -0700

    fix: create variable for return number random

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -6,5 +6,6 @@ import asyncio
 
 async def wait_random(max_delay: int = 10) -> float:
     """ function that shows a random floating """
+    value = random.uniform(0, max_delay)
     await asyncio.sleep(max_delay)
-    return random.uniform(0, max_delay)
+    return value
