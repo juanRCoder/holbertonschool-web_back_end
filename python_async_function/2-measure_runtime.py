f509ff38a62c709baf2298e110eea54a9b9cde75
juanRCoder <guillecarck@gmail.com>
2023-09-28 11:54:08 -0700

    feat: Add function that calculates the execution time of await_n

---
+++ python_async_function/2-measure_runtime.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+""" Script that returns the total execution time """
+import asyncio
+import time
+
+
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+def measure_time(n: int, max_delay: int) -> float:
+    """ function that measures the execution time of await_n """
+    start_time = time.time()
+    asyncio.run(wait_n(n, max_delay))
+    end_time = time.time()
+
+    total_time = end_time - start_time
+    return total_time / n
