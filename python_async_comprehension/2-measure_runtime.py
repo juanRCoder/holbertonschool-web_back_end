569cf5cefb2ff94c14a8f7d28fb83ee42899798b
juanRCoder <guillecarck@gmail.com>
2023-09-29 15:19:41 -0700

    feat: Add function thatCalculate the execution time of 4 asynchronous functions

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -2,19 +2,18 @@
 """ Async Comprehension """
 import asyncio
 import time
-from typing import List
 
 
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
-async def measure_runtime() -> List[float]:
+async def measure_runtime() -> float:
     """ return time total of the execution of asyn_comprehension 4 times """
     start_time = time.time()
 
     await asyncio.gather(*[async_comprehension() for _ in range(4)])
 
     end_time = time.time()
-    total_time = start_time - end_time
+    total_time = end_time - start_time
 
     return total_time
