2bd817d3ea101c0c7c4f9f6114467feb151e901f
juanRCoder <guillecarck@gmail.com>
2023-10-03 11:45:22 -0700

    feat: Add list of documents with a common attribute

---
+++ NoSQL/11-schools_by_topic.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" Script that have function that return a list of the documents"""
+
+
+def schools_by_topic(mongo_collection, topic):
+    """ function that returns the list of school having a specific topic """
+    col = mongo_collection.find({"topic": topic})
+    list_school = [doc for doc in col]
+
+    return list_school
