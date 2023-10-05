744120f9fe20360de433bdeb54d0610f79670fdc
juanRCoder <guillecarck@gmail.com>
2023-10-05 09:27:07 -0700

    fix: line too long (81 > 79 characters)

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-""" Script that return a tuple with start index and a final index of the list """
+""" Script that return a tuple with range of the list """
 
 
 def index_range(page, page_size):
