d74aa42ef2c3979259970ce725027b3489f5bf1a
juanRCoder <guillecarck@gmail.com>
2023-10-05 11:03:45 -0700

    fix: add function out class Server

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -6,6 +6,13 @@ import csv
 import math
 from typing import List, Tuple
 
+def index_range(page, page_size) -> Tuple[int, int]:
+    """ return tuple that have range index of the list
+    """
+    start_index = (page - 1) * page_size
+    last_index = page * page_size
+
+    return (start_index, last_index)
 
 class Server:
     """Server class to paginate a database of popular baby names.
@@ -14,14 +21,6 @@ class Server:
 
     def __init__(self):
         self.__dataset = None
-
-    def index_range(self, page, page_size) -> Tuple:
-        """ return tuple that have range index of the list
-        """
-        start_index = (page - 1) * page_size
-        last_index = page * page_size
-
-        return (start_index, last_index)
 
     def dataset(self) -> List[List]:
         """Cached dataset
@@ -42,13 +41,12 @@ class Server:
         assert isinstance(page_size, int) and page_size > 0
 
         # primera y ulima posicion de la pagina actual.
-        first_index, last_index = self.index_range(page, page_size)
+        first_index, last_index = index_range(page, page_size)
 
         # longitud de todos los datos en el archivo csv.
-        data = self.dataset()
-        data_size = len(data)
+        data_size = len(self.dataset())
 
         if first_index > data_size or last_index > data_size:
             return []
 
-        return data[first_index:last_index + 1]
+        return [self.dataset()[i] for i in range(first_index, last_index)]
