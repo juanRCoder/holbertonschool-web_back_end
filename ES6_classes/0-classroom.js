f1c5f4acd387f441fdf7a63ff4054eaec53fc263
juanRCoder <guillecarck@gmail.com>
2023-10-12 21:40:56 +0000

    fixing: adding conditional for check attribute number

--- ES6_classes/0-classroom.js
+++ ES6_classes/0-classroom.js
@@ -1,5 +1,8 @@
 export default class ClassRoom {
 	constructor(maxStudentsSize){
+		if (typeof maxStudentsSize !== 'number'){
+			throw new TypeError('Length must be a number');
+		}
 		this._maxStudentsSize = maxStudentsSize;
 	}
 }
