51064d17ba6069d5f9f1a22a545730ae0b20e223
juanRCoder <guillecarck@gmail.com>
2023-10-12 21:50:10 +0000

    fix: update class

--- ES6_classes/0-classroom.js
+++ ES6_classes/0-classroom.js
@@ -1,8 +1,8 @@
 export default class ClassRoom {
-	constructor(maxStudentsSize){
-		if (typeof maxStudentsSize !== 'number'){
-			throw new TypeError('Length must be a number');
-		}
-		this._maxStudentsSize = maxStudentsSize;
-	}
+    constructor(maxStudentsSize){
+        if (typeof maxStudentsSize !== 'number'){
+            throw TypeError('Length must be a number');
+        }
+	this._maxStudentsSize = maxStudentsSize;
+    }
 }
