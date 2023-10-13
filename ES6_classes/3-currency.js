c3dade06d43524a6732fe3ae3a618d77eae9735a
juanRCoder <guillecarck@gmail.com>
2023-10-13 18:14:13 +0000

    fix: adding (;) in conditionals in constructor

--- ES6_classes/3-currency.js
+++ ES6_classes/3-currency.js
@@ -1,7 +1,7 @@
 export default class Currency {
   constructor(code, name) {
-    if (typeof code !== 'string') throw TypeError('code must be a string')
-    if (typeof name !== 'string') throw TypeError('name must be a string')
+    if (typeof code !== 'string') throw TypeError('code must be a string');
+    if (typeof name !== 'string') throw TypeError('name must be a string');
     this._code = code;
     this._name = name;
   }
