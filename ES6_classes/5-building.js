fa4a67441ba1f5deb0427faaa135d78c004aac76
juanRCoder <guillecarck@gmail.com>
2023-10-13 22:09:45 +0000

    feat: adding method that extends in class child

---
+++ ES6_classes/5-building.js
@@ -0,0 +1,21 @@
+export default class Building {
+  constructor(sqft) {
+    if (typeof sqft !== 'number') throw TypeError('sqft must be a number');
+    this._sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(value) {
+    if (typeof value === 'number') this._sqft = value;
+    else throw TypeError('sqft must be a number');
+  }
+
+  evacuationWarningMessage() {
+    if (this.evacuationWarningMessage === Building.prototype.evacuationWarningMessage) {
+      throw Error('Class extending Building must override evacuationWarningMessage');
+    }
+  }
+}
