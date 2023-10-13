2a1be25699021fb64a4c6f7077bfcad28b5334cc
juanRCoder <guillecarck@gmail.com>
2023-10-13 16:58:15 +0000

    feat: adding methods specials for class

---
+++ ES6_classes/2-hbtn_course.js
@@ -0,0 +1,31 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+  
+  get name() {
+    return this._name;
+  }
+  set name(value) {
+    if (typeof value == 'string') this._name = value;
+    else console.error("name is not a string");
+  }
+
+  get length() {
+    return this._length;
+  }
+  set length(value) {
+    if (typeof value == 'number') this._length = value;
+    else console.error("length is not a number");
+  }
+
+  get students() {
+    return this._students;
+  }
+  set students(value) {
+    if (Array.isArray(value)) this._students = value;
+    else console.error("students is not a array");
+  }
+}
