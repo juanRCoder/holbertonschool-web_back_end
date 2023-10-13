28632e067792eafe40048dafd79018b9f0fd91d4
juanRCoder <guillecarck@gmail.com>
2023-10-13 16:30:08 +0000

    fix: Fixing the Code by Convention

--- ES6_classes/1-make_classrooms.js
+++ ES6_classes/1-make_classrooms.js
@@ -1,6 +1,6 @@
-import ClassRoom from "./0-classroom.js";
+import ClassRoom from './0-classroom';
 
 export default function initializeRooms() {
   const arr = [19, 20, 34];
-  return arr.map(x => new ClassRoom(x));
+  return arr.map((x) => new ClassRoom(x));
 }
