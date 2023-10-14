05cdb7bb552321df8b2560941b6067aa9681955d
juanRCoder <guillecarck@gmail.com>
2023-10-14 01:32:54 +0000

    adding class that Inherits Building

---
+++ ES6_classes/6-sky_high.js
@@ -0,0 +1,22 @@
+import Building from './5-building';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    if (typeof floors !== 'number') throw TypeError('floors must be a number');
+    this._floors = floors;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  set floors(value) {
+    if (typeof value === 'number') this._floors = value;
+    else throw TypeError('floors must be a number');
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this._floors} floors`;
+  }
+}
