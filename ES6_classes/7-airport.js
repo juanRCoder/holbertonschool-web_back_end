7bb12885299520d4a880076fb94e50dccb8cc386
juanRCoder <guillecarck@gmail.com>
2023-10-14 01:54:20 +0000

    feat: adding class with method string

---
+++ ES6_classes/7-airport.js
@@ -0,0 +1,12 @@
+export default class Airport {
+  constructor(name, code) {
+    if (typeof name !== 'string') throw TypeError('name must be string');
+    if (typeof code !== 'string') throw TypeError('code must be string');
+    this._name = name;
+    this._code = code;
+  }
+
+  toString() {
+    return `[object ${this._code}]`;
+  }
+}
