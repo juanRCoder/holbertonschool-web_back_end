d361afa6e3b5f367b3bf07fd0316f770364e38c5
juanRCoder <guillecarck@gmail.com>
2023-10-14 18:02:06 +0000

    feat: adding method that return new instance of the class

--- ES6_classes/10-car.js
+++ ES6_classes/10-car.js
@@ -1,14 +1,11 @@
 export default class Car {
   constructor(brand, motor, color) {
-    if (typeof brand !== 'string') throw TypeError('brand must be string');
-    if (typeof motor !== 'string') throw TypeError('motor must be string');
-    if (typeof color !== 'string') throw TypeError('color must be string');
     this._brand = brand;
     this._motor = motor;
     this._color = color;
   }
 
   cloneCar() {
-    return new this.constructor(this._brand, this._motor, this._color);
+    return new this.constructor();
   }
 }
