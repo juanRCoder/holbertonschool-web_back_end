a513782156b5efc7b55894321b0ae637400bd9a8
juanRCoder <guillecarck@gmail.com>
2023-10-17 02:53:23 +0000

    feat: ading for..of for get new arr with new elements

---
+++ ES6_basic/10-loops.js
@@ -0,0 +1,6 @@
+export default function appendToEachArrayValue(array, appendString) {
+  const newArr = [];  
+  for (let value of array) newArr.push(`${appendString}${value}`);
+
+  return newArr;
+}
