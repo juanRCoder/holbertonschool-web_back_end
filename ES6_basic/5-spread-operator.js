70d7cc86f428a9ee19bb5a454889f011073a2afe
juanRCoder <guillecarck@gmail.com>
2023-10-17 00:01:28 +0000

    feat: function that expand argument in elements

--- ES6_basic/5-spread-operator.js
+++ ES6_basic/5-spread-operator.js
@@ -1,3 +1,9 @@
 export default function concatArrays(array1, array2, string) {
   return [...array1, ...array2, ...string];
 }
+
+// export default function concatArrays(array1, array2, string) {
+//   let arr = array1.concat(array2);
+//   let str = Array.from(string);
+//   return arr.concat(str);
+//   }
