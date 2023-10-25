a2d7632c1fe0549a2c58eb32ba525fb7d43574a2
juanRCoder <guillecarck@gmail.com>
2023-10-25 23:33:34 +0000

    feat: async/await with two functions

--- ES6_promise/100-await.js
+++ ES6_promise/100-await.js
@@ -1,9 +1,9 @@
 import { uploadPhoto, createUser } from './utils';
 
 async function asyncUploadUser() {
-  const photo = await uploadPhoto();
-  const user = await createUser();
   try {
+   const photo = await uploadPhoto();
+   const user = await createUser();
     return { photo, user };
   } catch (e) {
     console.error(e);
