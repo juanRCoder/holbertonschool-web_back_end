ab9f932db30a1dece2b8227d463db4e398013819
juanRCoder <guillecarck@gmail.com>
2023-10-26 22:10:56 +0000

    fix: check that stratString be a string

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,9 +1,9 @@
 function cleanSet(set, startString) {
   const newString = [];
-  if (startString.length == 0) return '';
+  if (typeof startString !== 'string' || startString.length === 0) return '';
   for (const value of set) {
     if (value.startsWith(startString)) newString.push(value.slice(startString.length));
-  };
+  }
   return newString.join('-');
 }
 export default cleanSet;
