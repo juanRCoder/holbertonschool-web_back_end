8ce668a34e2e6a5302c1bfe09cb62af93f17d5ab
juanRCoder <guillecarck@gmail.com>
2023-10-26 22:05:49 +0000

    feat: clean characteres equals

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,8 +1,9 @@
-export default function cleanSet(set, startString) {
+function cleanSet(set, startString) {
   const newString = [];
+  if (startString.length == 0) return '';
   for (const value of set) {
-    if (startString.length === 0) break;
     if (value.startsWith(startString)) newString.push(value.slice(startString.length));
-  }
+  };
   return newString.join('-');
 }
+export default cleanSet;
