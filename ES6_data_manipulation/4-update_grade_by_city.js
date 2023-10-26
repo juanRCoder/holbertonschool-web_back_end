291268be5001d033d0ad5aa5ecd0403ff6752d0d
juanRCoder <guillecarck@gmail.com>
2023-10-26 20:29:20 +0000

    feat: list filter with location equal and add grade

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -2,8 +2,7 @@ export default function updateStudentGradeByCity(students, city, newGrade) {
   return students
     .filter((student) => student.location === city)
     .map((student) => {
-      const updateGrade = newGrade
-	.find((grade) => grade.studentId === student.id);
+      const updateGrade = newGrade.find((grade) => grade.studentId === student.id);
       const grade = updateGrade ? updateGrade.grade : 'N/A';
       return { ...student, grade };
     });
