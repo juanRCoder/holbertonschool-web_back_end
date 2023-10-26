fa4096f36b60348060df0625cecd9a0c567862fe
juanRCoder <guillecarck@gmail.com>
2023-10-26 22:14:04 +0000

    fix: check that set be a object

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,6 @@
 function cleanSet(set, startString) {
   const newString = [];
-  if (typeof startString !== 'string' || startString.length === 0) return '';
+  if (typeof set !== 'object' || typeof startString !== 'string' || startString.length === 0) return '';
   for (const value of set) {
     if (value.startsWith(startString)) newString.push(value.slice(startString.length));
   }
